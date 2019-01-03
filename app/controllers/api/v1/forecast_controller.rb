6a25599d02b2d7f9b2bb65bb0300d0187320b146
Ryan McNeil <ryan.mcneil288@gmail.com>
2019-01-02 20:13:27 -0700

    mock call to google api
    make faraday call in controller (to be refactored) and use webmock in text with location fixture

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -2,12 +2,16 @@ class Api::V1::ForecastController < ApplicationController
 
   def show
     coords = get_coords(params[:location])
+    render json: coords
 
   end
 
   private
 
   def get_coords(city)
-    
+    conn = Faraday.new(url: 'https://maps.googleapis.com')
+    response = conn.get("/maps/api/geocode/json?address=#{city}&key=#{ENV['GOOGLE_API_KEY']}")
+
+    location_data = JSON.parse(response.body, symbolize_names: true)[:results][0][:geometry][:location]
   end
 end
