bd53d37840b520f9bd36ac4d6e97c5e5f12bf901
Ryan McNeil <ryan.mcneil288@gmail.com>
2019-01-04 09:26:42 -0700

    write test
    wrote test to hit endpoint, and expected first day to have the time and summary of my darksky fixture

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,6 +15,7 @@ VCR.configure do |config|
   config.configure_rspec_metadata!
   config.filter_sensitive_data("<GOOGLE_API_KEY>") { ENV['GOOGLE_API_KEY'] }
   config.filter_sensitive_data("<DARKSKY_API_KEY>") { ENV['DARKSKY_API_KEY'] }
+  config.filter_sensitive_data("<GIPHY_API_KEY>") { ENV['DARKSKY_API_KEY'] }
 end
 
 begin
---
+++ spec/requests/api/v1/gifs_request_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'Giphy API' do
+  it 'sends gifs for each day in forecast' do
+    stub_request(:get, "https://maps.googleapis.com/maps/api/geocode/json?address=denver,co&key=#{ENV['GOOGLE_API_KEY']}").
+      to_return(body: File.read('spec/fixtures/location.json'))
+
+    coords = '39.7392358,-104.990251'
+    stub_request(:get, "https://api.darksky.net/forecast/#{ENV['DARKSKY_API_KEY']}/#{coords}?exclude=[minutely,flags,alerts]").
+      to_return(body: File.read('spec/fixtures/denver_forecast.json'))
+
+    get '/api/v1/gifs?location=denver,co'
+
+    expect(response.status).to eq 200
+
+    gif_forecast = JSON.parse(response.body, symbolize_names: true)
+
+    expect(gif_forecast[:data][:daily_forecasts].count).to eq 8
+    expect(gif_forecast[:data][:daily_forecasts][0][:time]).to eq 1546498800
+    expect(gif_forecast[:data][:daily_forecasts][0][:summary]).to eq "Clear throughout the day."
+    expect(gif_forecast[:data][:daily_forecasts][0][:url]).to eq ""
+  end
+end
