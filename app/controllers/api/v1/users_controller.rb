2f84b330b98e00bef523f6fb20c860033ed5b49f
Ryan McNeil <ryan.mcneil288@gmail.com>
2019-01-04 17:51:58 -0700

    quick refactor

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::UsersController < ApplicationController
 
   def create
-    if params[:password] == params[:password_confirmation]
+    if correct_password?
       user = User.create(user_params)
       render json: UserSerializer.new(user)
     end
@@ -11,5 +11,9 @@ class Api::V1::UsersController < ApplicationController
 
   def user_params
     params.permit(:email, :password)
+  end
+
+  def correct_password?
+    params[:password] == params[:password_confirmation]
   end
 end
